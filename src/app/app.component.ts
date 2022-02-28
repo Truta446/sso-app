ea0f97a9aa93ac8a8386c18c2b95e8fc5368dcf1
Juan Lopes <juan.versolato@outlook.com>
2022-02-28 10:49:14 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+
+import { authCodeFlowConfig } from './sso.config';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sso-app';
+
+  constructor(private oauthService: OAuthService) {
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn() {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  get token() {
+    const claims = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
+}
